1b786d8baba060c460fc55eaf3bba18f7ec6805f
erjo <erjo@cocoba.work>
2020-11-18 15:49:32 +0100

    [fix] new attempt to install wireguard

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -27,7 +27,7 @@ ynh_detect_arch(){
 	local architecture
 	if [ -n "$(uname -m | grep arm64)" ] || [ -n "$(uname -m | grep aarch64)" ]; then
 		architecture="arm64"
-	elif [ -n "$(uname -m | grep x86_64)" ]; then
+	elif [ -n "$(uname -m | grep x86_64)" ] || [ -n "$(uname -m | grep amd64)" ]; then
 		architecture="amd64"
 	elif [ -n "$(uname -m | grep 86)" ]; then
 		architecture="386"
