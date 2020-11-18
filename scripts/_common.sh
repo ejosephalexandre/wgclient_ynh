b178cee547882ce8af5c1a5b78f1b3f3132f0f92
erjo <erjo@cocoba.work>
2020-11-18 12:11:09 +0100

    Install on amd64

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -13,9 +13,32 @@ pkg_dependencies="wireguard-modules wireguard-dkms wireguard "
 # PERSONAL HELPERS
 #=================================================
 
+
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
+
+# Check the architecture
+# example: architecture=$(ynh_detect_arch)
+# usage: ynh_detect_arch
+# Requires YunoHost version 2.2.4 or higher.
+
+ynh_detect_arch(){
+	local architecture
+	if [ -n "$(uname -m | grep arm64)" ] || [ -n "$(uname -m | grep aarch64)" ]; then
+		architecture="arm64"
+	elif [ -n "$(uname -m | grep x86_64)" ]; then
+		architecture="amd64"
+	elif [ -n "$(uname -m | grep 86)" ]; then
+		architecture="386"
+	elif [ -n "$(uname -m | grep arm)" ]; then
+		architecture="arm"
+	else
+		architecture="unknown"
+	fi
+	echo $architecture
+}
+
 
 #=================================================
 # FUTURE OFFICIAL HELPERS
